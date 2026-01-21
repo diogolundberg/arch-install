3dcd1d9f9c7dbcfd0906277f8ffd0c6829773221
Diogo Lundberg <dclundberg@gmail.com>
2026-01-21 11:34:54 +0100

    Add hyprland configuration

--- scripts/installer/hypr.sh
+++ scripts/installer/hypr.sh
@@ -4,22 +4,22 @@ BASE_DIR=$(realpath "$(dirname "${BASH_SOURCE[0]}")/../../")
 source $BASE_DIR/scripts/installer/helper.sh
 
 log_message "Installation started for hypr section"
-print_info "\nStarting hypr setup..."
+print_info "\nStarting Hyprland setup..."
 print_info "\nEverything is recommended to INSTALL"
 
 check_root
 check_os
 
-run_command "pacman -S --noconfirm hyprland" "Install Hyprland (Must)" "yes"
+run_command "pacman -S --noconfirm --needed hyprland xorg-xwayland" "Install Hyprland + XWayland (Must for compatibility)" "yes"
+
+run_command "pacman -S --noconfirm --needed xdg-desktop-portal-hyprland" "Install XDG desktop portal for Hyprland" "yes"
+
+run_command "pacman -S --noconfirm polkit-gnome" "Install GNOME Polkit agent for authentication dialogs" "yes"
+
+run_command "pacman -S --noconfirm --needed qt5-wayland qt6-wayland" "Install Qt Wayland support (reduce XWayland fallback)" "yes"
+
 run_command "mkdir -p /home/$SUDO_USER/.config/hypr/ && cp -r $BASE_DIR/configs/hypr/hyprland.conf /home/$SUDO_USER/.config/hypr/" "Copy hyprland config (Must)" "yes" "no"
 
-run_command "pacman -S --noconfirm xdg-desktop-portal-hyprland" "Install XDG desktop portal for Hyprland" "yes"
-
-run_command "pacman -S --noconfirm polkit-kde-agent" "Install KDE Polkit agent for authentication dialogs" "yes"
-
-run_command "pacman -S --noconfirm dunst" "Install Dunst notification daemon" "yes"
-run_command "cp -r $BASE_DIR/configs/dunst /home/$SUDO_USER/.config/" "Copy dunst config" "yes" "no"
-
-run_command "pacman -S --noconfirm qt5-wayland qt6-wayland" "Install QT support on wayland" "yes"
+run_command "chown -R $SUDO_USER:$SUDO_USER /home/$SUDO_USER/.config/hypr" "Fix ownership for Hypr config" "no" "yes"
 
 echo "------------------------------------------------------------------------"
